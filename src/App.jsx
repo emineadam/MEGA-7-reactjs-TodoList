91fbd68f7f3a6db464820a874d2981abde263df9
emineadam <emineadamkonya@gmail.com>
2023-12-26 02:02:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper  />
+      <h6>MEGA Software Academy,Week 7 Assignment</h6> 
+      <h6>Prepared By: Emine ADAM </h6>
+    </div>
+    
+  );
+}
+
+
+export default App;
